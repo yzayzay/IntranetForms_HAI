7e119504d93a1ef752884699827a0ce83e339129
Yigbe Zayzay <yigbe.zayzay@gmail.com>
2018-10-19 15:03:14 -0500

    Update WebConfig.java

--- src/main/java/com/aws/codestar/projecttemplates/WebConfig.java
+++ src/main/java/com/aws/codestar/projecttemplates/WebConfig.java
@@ -1,4 +1,4 @@
-package com.app;
+package com.aws.codestar.projecttemplates;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
