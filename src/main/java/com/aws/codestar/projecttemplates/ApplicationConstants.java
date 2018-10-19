aaa687be042be979b57f609728db4333b98811f3
Yigbe Zayzay <yigbe.zayzay@gmail.com>
2018-10-19 15:02:28 -0500

    Create ApplicationConstants.java

---
+++ src/main/java/com/aws/codestar/projecttemplates/ApplicationConstants.java
@@ -0,0 +1,11 @@
+package com.app;
+
+public class ApplicationConstants {
+  public static final String NEW_CUSTOMER_REQUEST = "newCustomerRequest";
+  public static final String CREDIT_APPLICATION = "creditApplication";
+  public static final String NON_STANDARD_TERMS = "nonStandardTerms";
+  public static final String QUOTE_FORM = "quoteForm";
+  public static final String RTMC = "rtmc";
+  public static final String AUTH_RETURN_GOODS = "authReturnGoods";
+  public static final String PROSTEPREQUEST = "proStepRequest";
+}
