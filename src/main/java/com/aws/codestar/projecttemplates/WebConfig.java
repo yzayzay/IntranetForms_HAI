d58ab21874017cfb02d69247128d5a636ae3434f
Yigbe Zayzay <yigbe.zayzay@gmail.com>
2018-10-19 15:01:55 -0500

    Create WebConfig.java

---
+++ src/main/java/com/aws/codestar/projecttemplates/WebConfig.java
@@ -0,0 +1,22 @@
+package com.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.app.web")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+  public WebConfig() throws Exception {
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/**").addResourceLocations("file:ui/");
+  }
+
+}
